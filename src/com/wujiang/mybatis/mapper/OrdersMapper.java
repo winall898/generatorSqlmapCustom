101e0f4d089e539bddad1a76b6a2e54a06dadf46
wujiang <468086712@qq.com>
2018-09-13 17:32:00 +0800

    add

---
+++ src/com/wujiang/mybatis/mapper/OrdersMapper.java
@@ -0,0 +1,31 @@
+package com.wujiang.mybatis.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+import com.wujiang.mybais.pojo.Orders;
+import com.wujiang.mybais.pojo.OrdersExample;
+
+public interface OrdersMapper {
+    int countByExample(OrdersExample example);
+
+    int deleteByExample(OrdersExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Orders record);
+
+    int insertSelective(Orders record);
+
+    List<Orders> selectByExample(OrdersExample example);
+
+    Orders selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Orders record, @Param("example") OrdersExample example);
+
+    int updateByExample(@Param("record") Orders record, @Param("example") OrdersExample example);
+
+    int updateByPrimaryKeySelective(Orders record);
+
+    int updateByPrimaryKey(Orders record);
+}
---
+++ src/com/wujiang/mybatis/mapper/UserMapper.java
@@ -0,0 +1,31 @@
+package com.wujiang.mybatis.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+import com.wujiang.mybais.pojo.User;
+import com.wujiang.mybais.pojo.UserExample;
+
+public interface UserMapper {
+    int countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
